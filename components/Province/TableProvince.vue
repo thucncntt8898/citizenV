478b387fcb6f061414677b34fc154febdfaed147
thudt19 <thudt19@ghtk.internal>
2021-12-26 17:19:51 +0700

    update code

--- components/Province/TableProvince.vue
+++ components/Province/TableProvince.vue
@@ -34,7 +34,7 @@
           <div class="form-row align-items-center">
             <div class="col-sm-12 my-1">
               <button-custom class="btn-add" classIcon="fa fa-plus-circle" buttonName="Thêm mới"
-                             @submitEvent="createEvent()" v-if="user.role == 2 && checkUserPermission()"></button-custom>
+                             @submitEvent="createEvent()"></button-custom>
               <button-custom class="btn-filter" backgroundColor="#058f49" classIcon="fa fa-search"
                              :is-spinner="isLoadingProvince" @submitEvent="filter()"
                              buttonName="Tìm kiếm"></button-custom>
